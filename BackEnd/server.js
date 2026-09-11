48fc9007f67920bea8167d2a30917627f4dd975a
Tirth Patel <vasanitirth2900@gmail.com>
2026-09-11 17:19:22 +0530

    Initial commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -40,7 +40,7 @@ if(process.env.NODE_ENV === "production") {
 app.use(express.static(path.join(__dirname, '/FrontEnd/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'FrontEnd', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'FrontEnd', 'dist', 'index.html'));
 })
 }else {
   app.get('/', (req, res) => {
